51c118735d1a5bf2ad273eef8ce4439d415d5c8f
Morgan Hunt <to.morgan.hunt@gmail.com>
2016-03-08 15:24:44 -0700

    Finally have it functional.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 	$( "#cityfield" ).keyup(function(){
-		var url = "http://ec2-52-25-149-92.us-west-2.compute.amazonaws.com:3000/getcity?q="+$("#cityfield").val();
+		var url = "https://students.cs.byu.edu/~clement/CS360/ajax/getcity.cgi?q="+$("#cityfield").val();
 		$.ajax({url:url,dataType: "json",
 		success:function(data){
 			console.log("Got "+data);
@@ -27,12 +27,35 @@ $(document).ready(function(){
 
 		$("#txtHint").text("Keyup "+$("#cityfield").val());
 	});
+
+	$("#cityform").submit(function(e){
+		var value = $("#cityfield").val();
+		console.log(value);
+		e.preventDefault();
+		$("#dispcity").text(value);
+
+		var myurl = "https://api.wunderground.com/api/7f2a404372c3f011/geolookup/conditions/q/UT/";
+		myurl += value;
+		myurl += ".json";
+		console.log(myurl);
+		$.ajax({
+			url : myurl,
+			data : "jsonp",
+			success : function(parsed_json){
+				//console.log(data);
+				var location = parsed_json['location']['city'];
+				var temp_string = parsed_json['current_observation']['temperature_string'];
+				var current_weather = parsed_json['current_observation']['weather'];
+				everything = "<ul>";
+				everything += "<li>Location: " + location;
+				everything += "<li>Temperature: " + temp_string;
+				everything += "<li>Weather: " + current_weather;
+				everything += "</ul>";
+				$("#weather").html(everything);
+			}
+		})
+	});
+
 });
 
 
-$("#cityform").submit(function(e){
-	var value = $("#cityfield").val();
-	console.log(value);
-	e.preventDefault();
-	$("#dispcity").text(value);
-});
